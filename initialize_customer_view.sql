ccd3c46210a8539ecb584078c71c7fbfcc2e7554
Jonathan Mooney <moonejon@gmail.com>
2019-02-22 00:14:56 -0600

    cleanup

--- initialize_customer_view.sql
+++ initialize_customer_view.sql
@@ -1,15 +1,13 @@
-
 DROP DATABASE IF EXISTS bamazon;
 
 CREATE DATABASE bamazon;
 
-USE bamazon
-CREATE TABLE products(
-
+USE bamazon;
+CREATE TABLE products (
     id INTEGER NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(50),
     department_name VARCHAR(50),
     price INTEGER(10),
     stock_quantity INTEGER(10),
-    PRIMARY KEY(id)
+    PRIMARY KEY (id)
 );
