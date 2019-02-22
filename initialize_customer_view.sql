db416e7b396beaa8b8b680537793a06670b91bc5
Jonathan Mooney <moonejon@gmail.com>
2019-02-22 00:05:02 -0600

    creating customer_view database

---
+++ initialize_customer_view.sql
@@ -0,0 +1,15 @@
+
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon
+CREATE TABLE products(
+
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(50),
+    department_name VARCHAR(50),
+    price INTEGER(10),
+    stock_quantity INTEGER(10),
+    PRIMARY KEY(id)
+);
