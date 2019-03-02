cdc6673017c3d7e2675e41b988b4c2cf37b70e4b
Jonathan Mooney <moonejon@gmail.com>
2019-03-02 00:57:40 -0600

    SQL seed data file

---
+++ creating_mock_data_customer_view.sql
@@ -0,0 +1,13 @@
+USE bamazon;
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Audio Technica M50x", "Technology", 80.00, 15), 
+("Palomino Blackwing", "Art", 2.00, 12), 
+("AirPods", "Technology", 120.00, 40), 
+("Cough Drops", "Health", 3.00, 25),
+("Deodorant", "Grooming", 2.50, 30),
+("Pop-Tarts", "Food", 3.50, 15),
+("iPad Pro 12.9", "Technology", 1,200, 20),
+("Playstation Dual-Shock Controller", "Technology", 50.00, 50),
+("Count Chocula", "Food", 1.50, 50),
+("Chronicles of Narnia Box Set", "Books", 35.00, 15)
